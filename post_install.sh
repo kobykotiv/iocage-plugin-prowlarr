726f9fa105d06c5e9b92ce88389a5dc97b26246a
Martin Wilke <miwi@ixsystems.com>
2019-01-31 14:44:35 +0800

    #62991

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,8 @@
 sysrc -f /etc/rc.conf  radarr_enable="YES"
 
+#Ticket #62991
+chown -R radarr:radarr /usr/local/share/radarr
+
 #we need write permission to be able to write plugins update. #53127
 chmod 755 /usr/local/radarr
 
