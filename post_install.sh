9ec5985b3e65b66b643434d272d8b3ded1cced98
kobykotiv <kobykotiv@users.noreply.github.com>
2021-11-15 23:18:54 -0500

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,9 @@
-sysrc -f /etc/rc.conf  radarr_enable="YES"
+sysrc -f /etc/rc.conf  prowlarr_enable="YES"
 
 #Ticket #62991
-chown -R radarr:radarr /usr/local/share/radarr
+chown -R prowlarr:prowlarr /usr/local/share/prowlarr
 
 #we need write permission to be able to write plugins update. #53127
-chmod 755 /usr/local/radarr
+chmod 755 /usr/local/prowlarr
 
-service radarr start
+service prowlarr start
